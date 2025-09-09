6a5db879a33396f7ac22871c427b5abf01d3dff6
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-09 17:07:37 -0300

    feat: create config server project

---
+++ eventify-config-server/src/main/java/br/com/rodrigolopes/eventify_config_server/EventifyConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigolopes.eventify_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyConfigServerApplication.class, args);
+	}
+
+}
---
+++ eventify-config-server/src/test/java/br/com/rodrigolopes/eventify_config_server/EventifyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigolopes.eventify_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
