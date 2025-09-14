3e93cceb397e33094464ef13d36d1bcc96b7d15c
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-13 22:54:56 -0300

    feat: initialize eventify-api-gateway with Maven wrapper, Spring Boot application, and basic project structure

---
+++ eventify-api-gateway/src/main/java/com/rodrigoplopes/ms/eventify_api_gateway/EventifyApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoplopes.ms.eventify_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyApiGatewayApplication.class, args);
+	}
+
+}
---
+++ eventify-api-gateway/src/test/java/com/rodrigoplopes/ms/eventify_api_gateway/EventifyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoplopes.ms.eventify_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
